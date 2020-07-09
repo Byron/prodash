78f56c0ffa4bea77a6ab7458734ad2cd206ac3e8
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-07-09 10:44:47 +0800

    first sketch of conditional drawing of log messages

--- src/line/draw.rs
+++ src/line/draw.rs
@@ -1,4 +1,5 @@
 use crate::tree;
+use ansi_term::Color;
 use std::{io, ops::RangeInclusive};
 
 #[derive(Default)]
@@ -15,12 +16,75 @@ pub struct Options {
     pub colored: bool,
 }
 
-pub fn lines(_out: &mut impl io::Write, progress: &tree::Root, state: &mut State, config: &Options) -> io::Result<()> {
+mod utils {
+    use ansi_term::{ANSIGenericString, Style};
+
+    pub struct Brush {
+        may_paint: bool,
+        style: Option<Style>,
+    }
+
+    impl Brush {
+        pub fn new(colored: bool) -> Self {
+            Brush {
+                may_paint: colored,
+                style: None,
+            }
+        }
+
+        pub fn style(&mut self, style: Style) -> &mut Self {
+            self.style = Some(style);
+            self
+        }
+
+        #[must_use]
+        pub fn paint<'a, I, S: 'a + ToOwned + ?Sized>(&mut self, input: I) -> ANSIGenericString<'a, S>
+        where
+            I: Into<std::borrow::Cow<'a, S>>,
+            <S as ToOwned>::Owned: std::fmt::Debug,
+        {
+            match (self.may_paint, self.style.take()) {
+                (true, Some(style)) => style.paint(input),
+                (_, Some(_)) | (_, None) => ANSIGenericString::from(input),
+            }
+        }
+    }
+}
+
+fn messages(_out: &mut impl io::Write, messages: &[tree::Message], colored: bool) -> io::Result<()> {
+    let mut brush = utils::Brush::new(colored);
+    fn to_color(level: tree::MessageLevel) -> Color {
+        use tree::MessageLevel::*;
+        match level {
+            Info => Color::White,
+            Success => Color::Green,
+            Failure => Color::Red,
+        }
+    }
+    for tree::Message {
+        time: _,
+        level,
+        origin,
+        message,
+    } in messages
+    {
+        writeln!(
+            _out,
+            "{}→{}",
+            brush.style(Color::Yellow.dimmed()).paint(origin),
+            brush.style(to_color(*level).bold()).paint(message)
+        )?;
+    }
+    Ok(())
+}
+
+pub fn lines(out: &mut impl io::Write, progress: &tree::Root, state: &mut State, config: &Options) -> io::Result<()> {
     progress.sorted_snapshot(&mut state.tree);
     if !config.keep_running_if_progress_is_empty && state.tree.is_empty() {
         return Err(io::Error::new(io::ErrorKind::Other, "stop as progress is empty"));
     }
     state.from_copying = Some(progress.copy_new_messages(&mut state.messages, state.from_copying.take()));
+    messages(out, &state.messages, config.colored)?;
     if config.output_is_terminal {
         let level_range = config
             .level_filter
