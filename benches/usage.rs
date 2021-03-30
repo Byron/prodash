b423189f2ad3fdc111a17aca33aa17e15afe4c58
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-03-30 19:26:05 +0800

    Try glassbench
    Here are the findings:
    
    On prodash I measured 36.53s for building with Criterion and the the total elapsed time was 1:17m for running the benches.
    
    For glassbench, compilation took 37.4s and the total elapsed time was 43.9s.
    
    Missing: throughpu

--- benches/usage.rs
+++ benches/usage.rs
@@ -1,18 +1,18 @@
-use criterion::*;
+use glassbench::*;
 
 use prodash::{messages::MessageLevel, Tree, TreeOptions};
 
-fn usage(c: &mut Criterion) {
+fn usage(b: &mut Bench) {
     fn small_tree() -> Tree {
         TreeOptions {
             initial_capacity: 10,
             message_buffer_capacity: 2,
         }
         .create()
-    };
-    c.benchmark_group("Tree::add_child")
-        .throughput(Throughput::Elements(4))
-        .bench_function("add children to build a tree of tasks and clear them (in drop)", |b| {
+    }
+    b.task(
+        "Tree::add_child: add children to build a tree of tasks and clear them (in drop)",
+        |b| {
             let root = small_tree();
             b.iter(|| {
                 let mut c = root.add_child("1");
@@ -20,37 +20,34 @@ fn usage(c: &mut Criterion) {
                 let _two = c.add_child("2");
                 let _three = c.add_child("3");
             });
+        },
+    );
+    b.task("tree::Item::set: set tree 5 times", |b| {
+        let root = small_tree();
+        let mut progress = root.add_child("the one");
+        progress.init(Some(20), Some("element".into()));
+        b.iter(|| {
+            progress.set(1);
+            progress.set(2);
+            progress.set(3);
+            progress.set(4);
+            progress.set(5);
         });
-    c.benchmark_group("tree::Item::set")
-        .throughput(Throughput::Elements(5))
-        .bench_function("set tree 5 times", |b| {
+    });
+    b.task(
+        "tree::Item::message: send one message with a full message buffer (worst case performance)",
+        |b| {
             let root = small_tree();
             let mut progress = root.add_child("the one");
             progress.init(Some(20), Some("element".into()));
             b.iter(|| {
-                progress.set(1);
-                progress.set(2);
-                progress.set(3);
-                progress.set(4);
-                progress.set(5);
+                progress.message(MessageLevel::Success, "for testing");
             });
-        });
-    c.benchmark_group("tree::Item::message")
-        .throughput(Throughput::Elements(1))
-        .bench_function(
-            "send one message with a full message buffer (worst case performance)",
-            |b| {
-                let root = small_tree();
-                let mut progress = root.add_child("the one");
-                progress.init(Some(20), Some("element".into()));
-                b.iter(|| {
-                    progress.message(MessageLevel::Success, "for testing");
-                });
-            },
-        );
-    c.benchmark_group("Tree::copy_messages")
-        .throughput(Throughput::Elements(2))
-        .bench_function("copy all messages with buffer being at capacity", |b| {
+        },
+    );
+    b.task(
+        "Tree::copy_messages: copy all messages with buffer being at capacity",
+        |b| {
             let root = small_tree();
             let mut progress = root.add_child("the one");
             progress.init(Some(20), Some("element".into()));
@@ -60,8 +57,8 @@ fn usage(c: &mut Criterion) {
             b.iter(|| {
                 root.copy_messages(&mut out);
             });
-        });
+        },
+    );
 }
 
-criterion_group!(benches, usage);
-criterion_main!(benches);
+glassbench!("standard usage", usage,);
