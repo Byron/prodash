300181bdd4b2ef1822dddd1fe814d7e3e5b26779
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-10-17 16:58:50 +0800

    feat!: remove `Progress: 'static` requirement.
    This requirement can be added where used and where needed, and
    originally snuck in because it was easier and `Progress` implementations
    typically are `'static` as well.
    
    However, that requirement made it impossible to implement `Progoress`
    for `&mut T where T: Progress`.

--- src/traits.rs
+++ src/traits.rs
@@ -2,7 +2,7 @@ use crate::{messages::MessageLevel, progress, Unit};
 use std::time::Instant;
 
 /// A trait for describing hierarchical process.
-pub trait Progress: Send + 'static {
+pub trait Progress: Send {
     /// The type of progress returned by [`add_child()`][Progress::add_child()].
     type SubProgress: Progress;
 
